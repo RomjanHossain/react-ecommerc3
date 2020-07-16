87cd725c07e010b491a0408d6ea7de328f2dc11f
RomjanHossain <romjanhossain726526@gmail.com>
2020-07-16 12:54:06 +0500

    updated

--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -3,10 +3,14 @@ import "./menu-item.components.scss";
 
 // creating a functional compo [we don't need any state to handle for this xD]
 
-const MenuItem = () => (
-  <div className="menu-item">
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="backgroundImage"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
     <div className="content">
-      <h1 className="title">Hats</h1>
+      <h1 className="title">{title}</h1>
       <span className="subtitle">buy now</span>
     </div>
   </div>
