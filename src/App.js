19d6246ae09298e97045fa177086741eeac4ed2b
RomjanHossain <romjanhossain726526@gmail.com>
2020-07-22 18:06:16 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.components";
 import ShopList from "./pages/shop/shop.compo";
+import Header from "./components/header/header.components";
 
 function App() {
   return (
     <div className="App">
-      {/* <Homepage /> */}
-      <Route exact component={Homepage} path="/" />
-      <Route component={ShopList} path="/shop" />
+      <Header />
+      <Switch>
+        <Route exact component={Homepage} path="/" />
+        <Route component={ShopList} path="/shop" />
+      </Switch>
     </div>
   );
 }
--- src/components/preview/collection.preview.jsx
+++ src/components/preview/collection.preview.jsx
@@ -3,6 +3,7 @@ import "./collections.preview.scss";
 import CollectionItem from "../collection-item/collection-item.components";
 
 const CollectionPreview = ({ title, items }) => (
+  // <h2>collections</h2>
   <div className="collection-preview">
     <h1 className="title">{title}</h1>
     <div className="preview">
