804fa60a0a063cf28ee9c53a83a487909fd59671
RomjanHossain <romjan@skiff.com>
2020-07-23 01:08:17 +0600

    sign in and out page

---
+++ src/pages/signInandSignUp/signInandSignUp.components.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./signInandSignUp.components.scss";
+import SignIn from "../../components/signIn/signIn.components";
+const SignInandSignUp = () => (
+  <div className="sign-in-and-sing-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInandSignUp;
